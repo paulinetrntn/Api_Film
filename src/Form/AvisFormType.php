d4d66cecacd82852b948236ce0ca6bda3a515392
TRONTIN Pauline <morgane71480@gmail.com>
2023-10-19 22:56:14 +0200

    creation formulaire avis

---
+++ src/Form/AvisFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Avis;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AvisFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('idMovie')
+            ->add('idSerie')
+            ->add('username')
+            ->add('rates')
+            ->add('description')
+            ->add('date')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Avis::class,
+        ]);
+    }
+}
